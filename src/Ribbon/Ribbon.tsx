54b627ff8e4b1af2dcabd6d0a533f143b86337b5
Connor Skees <connor1skees@gmail.com>
2022-09-21 13:12:08 -0400

    play with styles in ribbon

--- src/Ribbon/Ribbon.tsx
+++ src/Ribbon/Ribbon.tsx
@@ -26,8 +26,8 @@ const NoteMap: Record<NoteValue, React.ReactNode> = {
             />
         </g>
     </svg>,
-    Quarter: <svg id="svg5" width="14.566" height="41.17" version="1.0" xmlns="http://www.w3.org/2000/svg">
-        <g id="g11065" transform="translate(-440.95 -9.4689)">
+    Quarter: <svg id="svg5" width="14.566" height="45" version="1.0" xmlns="http://www.w3.org/2000/svg">
+        <g id="g11065" transform="translate(-440.95 -5.6389)">
             <path id="path11056" d="m451.09 49.39c3.3958-1.82 5.2053-5.1146 4.0922-7.593-1.1873-2.6436-5.267-3.3897-9.1066-1.6654-3.8396 1.7244-5.9922 5.2694-4.8049 7.913 1.1873 2.6436 5.267 3.3897 9.1066 1.6654 0.23997-0.10777 0.48628-0.19874 0.71268-0.32007z" fillRule="evenodd" fill="currentColor" />
             <path id="path11058" d="m454.73 43.056v-33.588" fill="none" stroke="currentColor" strokeWidth="1.5" />
         </g>
